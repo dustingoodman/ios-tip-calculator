6099dbcc779f42eda3b17664e087a9047c93f7de
Dustin Goodman <dustin.goodman@capitalone.com>
2016-08-07 13:52:16 -0400

    Built base calculator

---
+++ tipnow/tipnow/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  tipnow
+//
+//  Created by Goodman, Dustin on 8/5/16.
+//  Copyright © 2016 Dustin Goodman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+        
+    }
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.20, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+
+}
+
